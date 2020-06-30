7c3a417a61d2f2a79e5366d3fca4a2fd28db6eca
Mauricio Sierra <mauricio.sierra@globant.com>
2020-06-30 07:24:44 -0500

    docs agregado

---
+++ docs/precache-manifest.bedd8a246aa49cf53b1cca9feb6f21e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025fd531f9e98411c1f3ac9a14f3c579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34c2a98a84cbff669f46",
+    "url": "/static/css/main.1bb7b96f.chunk.css"
+  },
+  {
+    "revision": "813340125952adac502d",
+    "url": "/static/js/2.8f5b0e35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f5b0e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c2a98a84cbff669f46",
+    "url": "/static/js/main.4a498ccc.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
